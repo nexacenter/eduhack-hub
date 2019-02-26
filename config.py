e853e4440d139ed60cc33f6bbda3a2d4a2f88228
Giovanni Garifo <giovanni.garifo@polito.it>
2019-02-26 16:32:48 +0000

    minor changes to tempates

--- config.py
+++
@@ -1,6 +0,0 @@
-password = 'prova'
-secret = '123upudnpomi8r2yn'
-db = 'sqlite:///edu.db'
-wp_pwd = 'provaprova2'
-email = 'me@francescomecca.eu'
-do_not_list = ['test', 'admin', 'atester'] # blogs that should not displayed in /blogs
